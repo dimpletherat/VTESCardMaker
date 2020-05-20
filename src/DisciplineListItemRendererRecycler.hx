c1756c91ee97b294b4f7628d26630add9727a619
dimpletherat <dimple_the_rat@yahoo.fr>
2020-05-20 16:48:01 +0200

    Create DisciplineListItemRendererRecycler.hx

---
+++ src/DisciplineListItemRendererRecycler.hx
@@ -0,0 +1,56 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.display.DisplayObjectContainer;
+import motion.easing.IEasing;
+import openfl.display.DisplayObject;
+import feathers.controls.dataRenderers.ItemRenderer;
+import entities.Discipline;
+import feathers.layout.HorizontalLayout;
+import feathers.data.ListViewItemState;
+import feathers.utils.DisplayObjectRecycler;
+import feathers.controls.Label;
+import feathers.controls.LayoutGroup;
+
+class DisciplineListItemRendererRecycler extends ItemRenderer
+{
+    public var _icon:DisciplineIcon;
+
+    public function new ()
+    {
+        super();
+        _icon = new DisciplineIcon();
+        icon = _icon;
+    }    
+    
+
+
+
+
+    public static function updateItem(item:ItemRenderer, state:ListViewItemState):Void
+    {
+       var d:Discipline = cast( state.data, Discipline );
+        /* trace("update", d.label, item.width);
+        item._icon.display( d, 30,30);*/
+        var iconSize:Int = 24;
+        cast(item.icon, DisciplineIcon).display( d,iconSize,iconSize);
+        item.text = d.label;
+
+        //trace("update", d.label, item.width);
+    };
+
+    public static function resetItem (item:ItemRenderer, state:ListViewItemState):Void
+    {
+        /*var d:Discipline = cast( state.data, Discipline );
+        item._icon.display(null);*/
+        item.text = "";
+
+        //trace("reset", d.label, item.width);
+    }
+/*
+    public static function destroy(item:DisciplineListItemRenderer):Void 
+    {
+        item.removeChild( item._icon);
+        item.removeChild( item._label);
+    }*/
+}
